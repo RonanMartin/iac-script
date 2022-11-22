e31bb9e86650c1c1fcc2d076b1dede66cc22cfd9
RonanMartin <ronanmartin86@outlook.com>
2022-11-22 14:50:55 +0000

    Incluso entrada nas pastas e apt-get em vez de somente apt

--- iacweb.sh
+++ iacweb.sh
@@ -2,27 +2,29 @@
 
 echo "Atualizando o sistema"
 
-apt update
-apt upgrade -y
+apt-get update
+apt-get upgrade -y
 
 echo "Instalando o APACHE"
 
-apt install apache2 -y
+apt-get install apache2 -y
 
 echo "Instalando o UNZIP"
 
-apt install unzip -y
+apt-get install unzip -y
 
 echo "Baixando aplicação no GitHub"
 
-wget -c -P /tmp https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+cd /tmp
+wget -c https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 
 echo "extraindo arquivos com UNZIP"
 
-unzip /tmp/*.zip
+unzip main.zip
 
 echo "Copiando arquivos para diretório APACHE"
 
-cp /tmp/linux-site-dio-main/* /var/www/html/ 
+cd linux-site-dio-main
+cp -R * /var/www/html/ 
 
 echo "Finalizado!"
